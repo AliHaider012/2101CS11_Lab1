4c8cdeac5afb13092588de6ab4f3c8ee3bca6339
anuj-2101cs11 <anuj_2101cs11@iitp.ac.in>
2022-07-27 18:08:21 +0530

    verify

--- sort/bubble.c
+++ sort/bubble.c
@@ -11,7 +11,6 @@ int main(void){
         scanf("%d", &arr[i]);
     }
 
-    //Bubble sort
     for (int i=0; i<n-1; i++){
         for (int j=0; j<n-i-1; j++){
             if (arr[j] > arr[j+1]){
