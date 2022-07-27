3e85a2f82ce81275c2221f480da2231024e03adb
anuj-2101cs11 <anuj_2101cs11@iitp.ac.in>
2022-07-27 18:17:42 +0530

    changed name of array

--- sort/quick.c
+++ sort/quick.c
@@ -45,15 +45,15 @@ void printArray(int array[], int size) {
 }
 
 int main() {
-  int data[] = {8, 7, 2, 1, 0, 9, 6};
+  int arr[] = {8, 7, 2, 1, 0, 9, 6};
   
-  int n = sizeof(data) / sizeof(data[0]);
+  int n = sizeof(arr) / sizeof(arr[0]);
   
   printf("Unsorted Array\n");
-  printArray(data, n);
+  printArray(arr, n);
   
-  quickSort(data, 0, n - 1);
+  quickSort(arr, 0, n - 1);
   
   printf("Sorted array in ascending order: \n");
-  printArray(data, n);
+  printArray(arr, n);
 }
